f54ee772d21aff590f71cdbbc4f6dbc7987ae9de
W01f <w01f@w01f.dev>
2020-12-10 07:39:37 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# w01f m00n
+
+w01f m00n is a terminal-based forensic utility. It searches the selected 
+path recursively for all or specific files and saves them in a csv file.
+
+The following implementations are planned:
+* selection of what to save in the csv file (file size, user, group ...)
+* text output of statistics
+* graphical output of statistics as images
+* deleting files with specific file extension
+* deleting files with specific file names
+* deleting files with specific file size
+* move or copy specific files to a directory
+* pack files as archive
+
+**the program is currently in the alpha-stage of development and is not 
+yet usable!**
---
+++ modules/program.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+"""w01f m00n
+Copyright (C) 2020 w01f - https://github.com/w01fdev/
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+########################################################################
+
+w01f hacks from Linux for Linux!
+
+fck capitalism, fck patriarchy, fck racism, fck animal oppression ...
+
+########################################################################
+"""
+
+
+# constants
+program_name = 'w01f m00n'
+program_website = 'https://github.com/w01fdev/m00n'
+program_date = '2020-12-10'
+program_version = '0.0.1'
