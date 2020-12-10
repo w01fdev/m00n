5c743713bde361538399c0e9656a9adc3e90126f
W01f <w01f@w01f.dev>
2020-12-10 11:55:34 +0100

    version 0.0.3 -> directory scanner created

--- main.py
+++ main.py
@@ -26,8 +26,54 @@ fck capitalism, fck patriarchy, fck racism, fck animal oppression ...
 """
 
 
+import os
+
+
+def directory_scanner(root):
+    """Scans the directory with all subdirectories.
+
+    :param root: <str>
+        The root directory from which the scan should start.
+    :return: <list>, <dict>
+        returns in first position a list containing the directories and
+        files. In second position a <dict> with 3 keys (directories,
+        files, total)
+    """
+
+    data = []
+    dirs_ix = 0
+    files_ix = 0
+    total_ix = 0
+
+    for dir_ix, (root, dirs, files) in enumerate(os.walk(root), start=1):
+        dirs_ix += 1
+        total_ix += 1
+        print(root)
+        data.append(root)
+
+        for filename in files:
+            files_ix += 1
+            total_ix += 1
+            path = os.path.join(root, filename)
+            print(path)
+            data.append(path)
+    else:
+        summary = {
+            'directories': dirs_ix,
+            'files': files_ix,
+            'total': total_ix,
+        }
+        print('\nscan executed: total: {:,} | directories: {:,} | files: {:,}'.format(
+            summary.get('total'), summary.get('directories'), summary.get('files'))
+        )
+
+    return data, summary
+
+
 def main():
     """Main function of the program."""
+
+    data, summary = directory_scanner('/home/anonymous/Pictures/W01f/pass/')
 
 
 if __name__ == '__main__':
